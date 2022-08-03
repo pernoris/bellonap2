210f7cd5040c2dc55d26b8f3e8b7a7e498d716a0
pernoris <99809646+pernoris@users.noreply.github.com>
2022-08-03 02:34:58 -0300

    app.module.ts/app-routing.module.ts

--- bellonap2/src/app/app-routing.module.ts
+++ bellonap2/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgCircleProgressModule.forRoot({})],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
