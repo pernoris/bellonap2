03b40248d02421f51b2c122809c1860b6321070e
pernoris <99809646+pernoris@users.noreply.github.com>
2022-08-04 10:08:51 -0300

    components/proyecto

--- bellonap2/src/app/app.module.ts
+++ bellonap2/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 
 @NgModule({
@@ -24,7 +26,9 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    FooterComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
