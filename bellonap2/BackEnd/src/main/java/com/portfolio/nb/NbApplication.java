526ca70630c9149fdfee7c60a655c9f1c5be3b22
pernoris <99809646+pernoris@users.noreply.github.com>
2022-08-06 12:15:42 -0300

    backend

---
+++ bellonap2/BackEnd/src/main/java/com/portfolio/nb/NbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.nb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbApplication.class, args);
+	}
+
+}
---
+++ bellonap2/BackEnd/src/test/java/com/portfolio/nb/NbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.nb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
