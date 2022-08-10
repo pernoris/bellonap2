88323021918f5ce270cafe115d18ebea5348bc2d
pernoris <99809646+pernoris@users.noreply.github.com>
2022-08-10 02:09:02 -0300

    model y service

--- bellonap2/FrontEnd/src/app/app.module.ts
+++ bellonap2/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
