c47d35e43527630e6f9f9ad02b40ae2bbf1150a9
pernoris <99809646+pernoris@users.noreply.github.com>
2022-08-18 02:17:14 -0300

    fix cargar nombre

--- bellonap2/BackEnd/src/main/java/com/portfolio/nb/Controller/PersonaController.java
+++ bellonap2/BackEnd/src/main/java/com/portfolio/nb/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -54,4 +54,11 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;        
     }
+    
+       @GetMapping("/personas/traer/perfil")
+    public Persona findPersona() {
+        return ipersonaService.findPersona((long) 1);
+
+    }
+
 }
